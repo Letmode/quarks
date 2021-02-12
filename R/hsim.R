aaea603b61be726b4b20c4ad92f7639780908042
Firlef <78929239+Firlef@users.noreply.github.com>
2021-02-12 16:36:04 +0100

    Update hsim.R
    added switch case statement

--- R/hsim.R
+++ R/hsim.R
@@ -51,3 +51,25 @@ hsim <- function(x, p = 0.95, method = c("age", "basic"), lambda = 0.98)
   results <- cbind(VaR = VaR, ES = ES)
   results
 }
+#' sollte so funzen, aber kp ;-)
+if (all(method == c("age", "basic"))) method = "basic"
+
+results <- switch(method,
+	basic = {
+		l <- sort(-x)
+		VaR <- stats::quantile(l, p, na.rm = TRUE)
+		ES <- mean(l[l >= VaR[1]])
+		list(VaR = VaR, ES = ES)
+	  },
+	age = {
+		n <- length(x)
+		w <- lambda ^ ((1:n) - 1) * (1 - lambda) / (1 - lambda ^ n)
+		l <- sort(-x)
+		l.ind <- order(-x)
+		pcum <- cumsum(w[l.ind])
+		VaR.ind <- which(pcum > p)[1]
+		VaR <- l[VaR.ind]
+		ES <- mean(l[l >= VaR])
+		list(VaR = VaR, ES = ES)
+	  })
+}
