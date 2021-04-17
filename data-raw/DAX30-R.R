7a378b30524bf8e90bf10ae8d06276c87cc81590
Sebastian Letmathe <sebastian-letmathe@t-online.de>
2021-04-17 11:04:01 +0200

    doch wieder alte Daten, da Beispiele nicht so schön aussehen

--- data-raw/DAX30-R.R
+++ data-raw/DAX30-R.R
@@ -1,6 +1,6 @@
 ## code to prepare `DAX30.R` dataset goes here
 DAX30 <- BatchGetSymbols::BatchGetSymbols("^GDAXI",
-  first.date = "2000-01-01", last.date = "2021-03-31",
+  first.date = "2000-01-01", last.date = "2020-12-31",
   freq.data = "daily"
 )$df.tickers
 usethis::use_data(DAX30, overwrite = TRUE)
