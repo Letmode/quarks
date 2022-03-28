26ba1683718508d9f02cda42b2ac4aadcb0a0226
Sebastian Letmathe <sebastian-letmathe@t-online.de>
2022-03-28 18:51:51 +0200

    preparation patch 1.0.12

--- R/plop.R
+++ R/plop.R
@@ -3,8 +3,11 @@
 #' Calculates portfolio returns or losses by assigning weights
 #'
 #' @param x a numeric matrix of asset returns or losses
-#' @param wts standardized portfolio weights (should add up to 1); by default
-#' the portfolio is equally weighted
+#' @param wts a numeric vector or matrix containing the standardized portfolio
+#' weights; by default the portfolio is equally weighted
+#' over time and across all assets; if a vector is passed to \code{wts} the
+#' portfolio is equally weighted over time; weights should add up to 1
+#' (row-wise for matrices)
 #' @param approxim controls if a first-order approximation for the calculation
 #' of returns or losses is used; default is \code{'lin'} (first-order
 #' approximation is employed)
@@ -37,13 +40,14 @@ plop <-  function(x, wts = NULL, approxim = c(0, 1)) {
   x <- as.matrix(x)
   ncol <- dim(x)[2]
   if (is.null(wts)) wts <- rep(1 / ncol, ncol)
-
+  if (dim(wts)[1] == 1 || is.null(dim(wts)[1])) op <- '%*%'
+  if (!is.null(dim(wts)[1]) && dim(wts)[1] > 1) op <- '*'
   switch(as.character(approxim),
          "0" =  {
-           pl <- apply(x, 1, FUN = function(x, wts) exp(x) %*% wts - 1, wts)
+           pl <- rowSums(get(op)(exp(x), wts) - 1)
          },
          "1" = {
-           pl <- apply(x, 1, FUN = function(x, wts) x %*% wts, wts)
+           pl <- rowSums(get(op)(x, wts))
          })
   return(list(pl = pl,
               wts = wts))
