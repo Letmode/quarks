45a5d29736d255e9604569d49bc455784cc1f6c8
Sebastian Letmathe <sebastian-letmathe@t-online.de>
2022-08-01 07:57:54 +0200

    app update

--- R/runFTSdata.R
+++ R/runFTSdata.R
@@ -5,7 +5,8 @@
 #' @import ggplot2
 
 runFTSdata <- function() {
-  app <- list(ui = fluidPage(useShinyjs(),
+  app <- suppressWarnings(
+    list(ui = fluidPage(useShinyjs(),
                               sidebarLayout(
                                 sidebarPanel(
                                   selectizeInput("ticker",
@@ -33,12 +34,12 @@ runFTSdata <- function() {
                                   p(actionButton("closeApp", "Schließen", icon = icon("power-off"), verify_fa = FALSE), br(),
                                     "Anwendung wird beendet.",
                                     style = "font-family: 'consolas'; font-si16pt"),
-                                  br(),
-                                  p(actionButton("histApp", "Zeigen/Ausblenden Dichtefunktion", icon = icon("eye"), verify_fa = FALSE), br(),
-                                    "Geschätzte Dichtefunktion der Renditen wird angezeigt/ausgeblendet.",
-                                    style = "font-family: 'consolas'; font-si16pt"),
-                                  br(),
-                                  plotOutput("hist")
+                                  br()
+                                  # p(actionButton("histApp", "Zeigen/Ausblenden Dichtefunktion", icon = icon("eye"), verify_fa = FALSE), br(),
+                                  #   "Geschätzte Dichtefunktion der Renditen wird angezeigt/ausgeblendet.",
+                                  #   style = "font-family: 'consolas'; font-si16pt"),
+                                  # br(),
+                                  # plotOutput("hist")
                                 ),
 
                                 mainPanel(br(),
@@ -51,6 +52,7 @@ runFTSdata <- function() {
                               )
   ),
   server = function(input, output, session){
+    options(warn = -1)
     updateSelectizeInput(session, "ticker",
                          choices = c(paste0(quarks:::ticker_stocks$Ticker[-1], " (", quarks:::ticker_stocks$Name[-1], ")"),
                                      paste0(quarks:::ticker_indices$Ticker, " (", quarks:::ticker_indices$Name, ")")),
@@ -70,6 +72,8 @@ runFTSdata <- function() {
         )
       )
     })
+
+
     output$plot1 <- renderDygraph({
       req(input$ticker)
       req(input$Datum[1])
@@ -88,8 +92,9 @@ runFTSdata <- function() {
       req(input$Datum[1])
       req(input$Datum[2])
       req(dataInput())
+
       dat2 = dataInput()[-1, ]
-      plot_data2 = xts::xts(dat2$ret.closing.price, dat2$ref.date)
+      plot_data2 = suppressMessages(xts::xts(dat2$ret.closing.price, dat2$ref.date))
       dygraphs::dygraph(plot_data2, main = paste(input$ticker, "- Log-Renditen (täglich)"),
                         ylab = "Y<sub>t</sub>", xlab = "Tägliche Beobachtungen (t)", group = "FZS") %>%
         dygraphs::dyRangeSelector(height = 20) %>%
@@ -111,30 +116,33 @@ runFTSdata <- function() {
       stopApp()
     })
 
-    shinyjs::toggle("hist")
-    observeEvent(input$histApp, {
-      output$hist <- renderPlot({
-        req(input$ticker)
-        req(input$Datum[1])
-        req(input$Datum[2])
-        req(dataInput())
-        dat2 = dataInput()[-1, ]
-        ggplot(dat2, aes(x = dat2$ret.closing.price)) +
-          geom_histogram(aes(y = ..density..), fill = "steelblue", binwidth = 0.0025, alpha = 0.2) +
-          geom_density(aes(color = "Kernel")) +
-          stat_function(aes(color = "Normal"), fun = dnorm,
-                        args = list(mean = mean(dat2$ret.closing.price), sd = sd(dat2$ret.closing.price))) +
-          theme(legend.position = c(0.115, 0.7), axis.title.y = element_blank(),
-                panel.background = element_rect(fill = 'white', color = 'black'),
-                panel.grid.major = element_line(color = 'darkgrey'),
-                legend.text = element_text(size = 11),
-                legend.title = element_text(size = 12)) +
-          scale_colour_manual("Dichte", values = c("red", "blue")) +
-          labs(x = expression(Y[t]))
-      })
-      shinyjs::toggle("hist")
-    })
-  }
+    # shinyjs::toggle("hist")
+    # observeEvent(input$histApp, {
+    #   output$hist <- renderPlot({
+    #     req(input$ticker)
+    #     req(input$Datum[1])
+    #     req(input$Datum[2])
+    #     req(dataInput())
+    #     dat2 = dataInput()[-1, ]
+    #     ggplot(dat2, aes(x = dat2$ret.closing.price)) +
+    #       geom_histogram(aes(y = ..density..), fill = "steelblue", binwidth = 0.0025, alpha = 0.2) +
+    #       geom_density(aes(color = "Kernel")) +
+    #       stat_function(aes(color = "Normal"), fun = dnorm,
+    #                     args = list(mean = mean(dat2$ret.closing.price), sd = sd(dat2$ret.closing.price))) +
+    #       theme(legend.position = c(0.115, 0.7), axis.title.y = element_blank(),
+    #             panel.background = element_rect(fill = 'white', color = 'black'),
+    #             panel.grid.major = element_line(color = 'darkgrey'),
+    #             legend.text = element_text(size = 11),
+    #             legend.title = element_text(size = 12)) +
+    #       scale_colour_manual("Dichte", values = c("red", "blue")) +
+    #       labs(x = expression(Y[t]))
+    #   })
+    #   shinyjs::toggle("hist")
+    # })
+
+    onStop(function() options(warn = 0))
+    }
+)
 )
 shiny::runApp(app)
 }
