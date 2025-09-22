7d2a869a60d8fd1d580994f0aa822d9e42de2e8d
Sebastian Letmathe <sebastian-letmathe@t-online.de>
2025-09-22 11:50:18 +0200

    update of data sets

--- cran-comments.md
+++ cran-comments.md
@@ -1,10 +1,11 @@
-## Patch release (version 1.1.3)
+## Patch release (version 1.1.4)
 This is a patch release.
 
-- fixed a bug in 'cvgtest()'
+- updated data sets
+- fixed some minor bugs
 
 ## Test environments
-* local R installation, R 4.2.0
+* local R installation, R 4.4.0
 * win-builder (devel)
 * r-hub
 
--- data-raw/data.R
+++ data-raw/data.R
@@ -1,6 +1,44 @@
 ## code to prepare datasets tickers: ^GDAXI, ^GSPC, ^N225, ^FTSE, ^HSI. ^DJI
-HSI <- BatchGetSymbols::BatchGetSymbols("^DJI",
-  first.date = "2000-01-01", last.date = "2021-12-31",
-  freq.data = "daily"
-)$df.tickers
+
+
+SP500 <- yfR::yf_get("^GSPC",
+  first_date = "2000-01-01",
+  last_date = "2023-12-31",
+  freq_data = "daily"
+)#$df.tickers
+usethis::use_data(SP500, overwrite = TRUE)
+
+HSI <- yfR::yf_get("^HSI",
+                     first_date = "2000-01-01",
+                     last_date = "2023-12-31",
+                     freq_data = "daily"
+)#$df.tickers
 usethis::use_data(HSI, overwrite = TRUE)
+
+DAX <- yfR::yf_get("^GDAXI",
+                     first_date = "2000-01-01",
+                     last_date = "2023-12-31",
+                     freq_data = "daily"
+)#$df.tickers
+usethis::use_data(DAX, overwrite = TRUE)
+
+NIK225 <- yfR::yf_get("^N225",
+                     first_date = "2000-01-01",
+                     last_date = "2023-12-31",
+                     freq_data = "daily"
+)#$df.tickers
+usethis::use_data(NIK225, overwrite = TRUE)
+
+DJI <- yfR::yf_get("^DJI",
+                     first_date = "2000-01-01",
+                     last_date = "2023-12-31",
+                     freq_data = "daily"
+)#$df.tickers
+usethis::use_data(DJI, overwrite = TRUE)
+
+FTSE100 <- yfR::yf_get("^FTSE",
+                     first_date = "2000-01-01",
+                     last_date = "2023-12-31",
+                     freq_data = "daily"
+)#$df.tickers
+usethis::use_data(FTSE100, overwrite = TRUE)
